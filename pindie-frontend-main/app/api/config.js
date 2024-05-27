747ae2beeae5ea7fffa443dc7019824229ea6ec2
Zabava2 <119228478+Zabava2@users.noreply.github.com>
2024-05-27 13:27:24 +0300

    Update config.js

--- pindie-frontend-main/app/api/config.js
+++ pindie-frontend-main/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://serverpindie.nomoredomainswork.ru/";
+export const BASE_URL = "https://serverpindie.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
